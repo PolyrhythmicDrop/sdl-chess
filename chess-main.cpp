f59ac27f56914dbf10bb91225682332812b6f04d
PolyrhythmicDrop <danananana82@gmail.com>
2024-04-17 14:39:57 -0500

    Implement main loop framework

---
+++ chess-main.cpp
@@ -0,0 +1,14 @@
+// Main game loop for SDL Chess
+
+// Includes, using SDL, SDL_image, standard IO, and strings
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+// ** Main loop **
+int main(int argc, char* args[])
+{
+
+
+}
