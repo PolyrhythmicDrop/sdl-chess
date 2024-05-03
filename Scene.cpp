b682de247201da819d76e2fb586c5464d0f50ab2
PolyrhythmicDrop <danananana82@gmail.com>
2024-05-03 15:40:09 -0500

    Scene base class

--- Scene.cpp
+++ Scene.cpp
@@ -1,1 +1,21 @@
 #include "Scene.h"
+
+Scene::Scene()
+{
+
+}
+
+Scene::~Scene()
+{
+
+}
+
+void Scene::addObject(GameObject* object)
+{
+	_objectVector.push_back(object);
+}
+
+void Scene::addToSceneDrawVector(IDrawable* object, std::function<void(int, int, int, int)> drawFunction)
+{
+	_sceneDrawVector.push_back(drawFunction);
+}
--- Scene.h
+++ Scene.h
@@ -1,5 +1,46 @@
 #pragma once
+#include <vector>
+#include "GameObject.h"
+#include "IDrawable.h"
+#include <functional>
+
+/// <summary>
+/// Abstract class for building scenes.
+/// </summary>
 class Scene
 {
+private:
+	/// <summary>
+	/// Vector containing all the objects in the scene.
+	/// </summary>
+	std::vector<GameObject*> _objectVector;
+
+	/// <summary>
+	/// Vector containing all the draw functions of the objects in the scene, in the order that they are to be drawn.
+	/// </summary>
+	std::vector<std::function<void(int, int, int, int)>> _sceneDrawVector;
+
+public:
+
+	Scene();
+	~Scene();
+
+	/// <summary>
+	/// Build the scene up by adding objects and constructing the draw vector with those objects.
+	/// </summary>
+	/// <returns></returns>
+	virtual Scene* buildScene() = 0;
+
+	/// <summary>
+	/// Adds an object to the back of to the scene object vector.
+	/// </summary>
+	/// <param name="object">The object to add to the vector.</param>
+	virtual void addObject(GameObject* object);
+
+	/// <summary>
+	/// Add draw functions to the scene draw vector.
+	/// </summary>
+	/// <param name="object">The object whose draw function will be added.</param>
+	virtual void addToSceneDrawVector(IDrawable* object, std::function<void(int, int, int, int)> drawFunction);
 };
 
