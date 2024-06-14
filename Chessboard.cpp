972a0657289ef1fca90040b42a78dd6b339439e1
PolyrhythmicDrop <danananana82@gmail.com>
2024-06-13 22:52:49 -0500

    remove extraneous buildChessboard lines

--- Chessboard.cpp
+++ Chessboard.cpp
@@ -88,7 +88,6 @@ Square Chessboard::createSquare(std::string name)
 
 void const Chessboard::buildChessboard()
 {
-	/*initializeBoardGrid();*/
 
 	SDL_Point boardBottomLeft = setBoardDimensionsToWindow();
 
@@ -105,7 +104,7 @@ void const Chessboard::buildChessboard()
 
 	auto nameItr = sqN.begin();
 	bool odd = true;
-	// Set the board index for every square in the board
+	
 	for (int row = 0; row < 8; ++row)
 	{
 		_boardGrid.push_back(std::vector<Square>{});
@@ -128,181 +127,6 @@ void const Chessboard::buildChessboard()
 			_boardGrid.at(row).at(column).setBoardIndex(row, column);
 		}
 	}
-
-	
-
-	//// *** //
-	//// Create the first row of squares
-
-	//std::vector<std::string> sqNames{ "a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1" };
-	//std::vector<Square> row1Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row1Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 1 Vector!";
-
-	//setRowTileType(true, row1Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row1Vect, 1);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the second row of squares
-
-	//sqNames = { "a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2" };
-	//std::vector<Square> row2Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row2Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 2 Vector!";
-
-	//setRowTileType(false, row2Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row2Vect, 2);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the third row of squares
-	//sqNames = { "a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3" };
-	//std::vector<Square> row3Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row3Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 3 Vector!";
-
-	//setRowTileType(true, row3Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row3Vect, 3);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the fourth row of squares
-	//sqNames = { "a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4" };
-	//std::vector<Square> row4Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row4Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 4 Vector!";
-
-	//setRowTileType(false, row4Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row4Vect, 4);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the fifth row of squares
-	//sqNames = { "a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5" };
-	//std::vector<Square> row5Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row5Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 5 Vector!";
-
-	//setRowTileType(true, row5Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row5Vect, 5);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the sixth row of squares
-	//sqNames = { "a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6" };
-	//std::vector<Square> row6Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row6Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 6 Vector!";
-
-	//setRowTileType(false, row6Vect);
-	//
-	//setRowSquarePosition(boardBottomLeft, row6Vect, 6);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the seventh row of squares
-	//sqNames = { "a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7" };
-	//std::vector<Square> row7Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row7Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 7 Vector!";
-
-	//setRowTileType(true, row7Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row7Vect, 7);
-
-	//sqNames.clear();
-	//// *** //
-
-	//// *** //
-	//// Create the eigth row of squares
-	//sqNames = { "a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8" };
-	//std::vector<Square> row8Vect{};
-
-	//for (int i = 0; i < sqNames.size(); ++i)
-	//{
-	//	row8Vect.push_back(createSquare(sqNames[i]));
-	//}
-
-	//LOG(TRACE) << "Squares added to Row 8 Vector!";
-
-	//setRowTileType(false, row8Vect);
-
-	//setRowSquarePosition(boardBottomLeft, row8Vect, 8);
-
-	//sqNames.clear();
-	//// *** //
-	//
-	//// Add the row vectors to the board grid vector
-	//_boardGrid.push_back(row1Vect);
-	//_boardGrid.push_back(row2Vect);
-	//_boardGrid.push_back(row3Vect);
-	//_boardGrid.push_back(row4Vect);
-	//_boardGrid.push_back(row5Vect);
-	//_boardGrid.push_back(row6Vect);
-	//_boardGrid.push_back(row7Vect);
-	//_boardGrid.push_back(row8Vect);
-
-	//// Set the board index for every square in the board
-	//for (int row = 0; row < _boardGrid.size(); ++row)
-	//{
-	//	for (int column = 0; column < _boardGrid[row].size(); ++column)
-	//	{
-	//		_boardGrid[row][column].setBoardIndex(row, column);
-	//	}
-	//}
 
 }
 
